89751b79faf64cc6692b5f983411a2f6d0141c29
Patrick Mukumbu <38749014+patrickmukumbu@users.noreply.github.com>
2022-03-09 17:05:18 +0300

    NOSTORY: First IAM role module commit (#1)
    * NOSTORY: First IAM role module commit
    
    * NOSTORY: Modified default for policy variable
    
    * NOSTORY: Added dynamicity for inline policy and policy resource

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "main" {
+  name                  = var.name
+  assume_role_policy    = var.assume_role_policy
+  description           = var.description
+  force_detach_policies = var.force_detach_policies
+  managed_policy_arns   = var.managed_policy_arns
+  max_session_duration  = var.max_session_duration
+  name_prefix           = var.name_prefix
+  path                  = var.path
+  permissions_boundary  = var.permissions_boundary
+  dynamic "inline_policy" {
+    for_each = var.inline_policy
+
+    content {
+      name   = lookup(inline_policy.value, "name")
+      policy = lookup(inline_policy.value, "policy")
+    }
+  }
+  tags = {
+    name        = var.name
+    environment = var.environment
+  }
+}
+
+resource "aws_iam_policy" "main" {
+  count       = var.create_policy ? 1 : 0
+  name        = var.policy_name
+  path        = var.path
+  description = var.policy_description
+  policy      = var.policy
+
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  count      = var.create_policy ? 1 : 0
+  role       = aws_iam_role.main.name
+  policy_arn = join("", aws_iam_policy.main.*.arn)
+}
+
+resource "aws_iam_role_policy_attachment" "managed" {
+  count      = length(var.managed_policy_arns) > 0 ? length(var.managed_policy_arns) : 0
+  role       = aws_iam_role.main.name
+  policy_arn = element(aws_iam_policy.main.*.arn, count.index)
+}
