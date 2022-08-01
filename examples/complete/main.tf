922846f84d08c2a3b76a0ecdfb572bea99ba0f4b
John Mwangi <35805602+ndegwajohn@users.noreply.github.com>
2022-08-01 14:58:04 +0300

    Option to create multiple policies for iam-role (#9)
    * NOSTORY: option to create multiple policies for iam-role
    
    * NOSTORY: added sids to policies and fixed typos
    
    * NOSTORY: outputs description update
    
    * NOSTORY: outputs description update

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,38 +1,25 @@
 
 module "complete_managed_policy" {
-  source                           = "./../../"
-  name                             = "example-complete-role"
-  assume_role_policy               = data.aws_iam_policy_document.ec2_assume_role_policy.json
-  description                      = "Example completerole with a variety of permissions"
-  managed_policy_arns              = ["arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]
-  force_detach_policies            = true
-  policy_name                      = "Example-customer-managed-policy"
-  permissions_boundary_policy_name = "Example-permission-boundary"
-  permissions_boundary_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:DescribeInstances",
-          "ec2:DescribeInstanceTypeOfferings",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeInstanceTypes",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeInstanceStatus"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["ec2:Describe*"]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+  source                = "./../../"
+  name                  = local.name
+  assume_role_policy    = data.aws_iam_policy_document.ec2_assume_role_policy.json
+  description           = "Example complete role with a variety of permissions"
+  managed_policy_arns   = ["arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"]
+  force_detach_policies = true
+  tags                  = local.tags
+  permissions_boundary  = module.boundary_policy.policy_arn
+  policies = {
+    "${local.name}-policy" = {
+      policy = local.role_policy
+      tags   = local.tags
+    }
+  }
+}
+
+module "boundary_policy" {
+  source      = "boldlink/iam-policy/aws"
+  version     = "1.0.2"
+  policy_name = "${local.name}-permissions-boundary"
+  description = "Example-permission-boundary"
+  policy      = local.permissions_boundary_policy
 }
